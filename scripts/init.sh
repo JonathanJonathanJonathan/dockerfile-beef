b3ffc3706f701e4752433d66e19f9898eca9dcc9
Matteo Rosi <mrosi@omniagroup.it>
2017-11-17 17:03:37 +0100

    change default credentials

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-source /usr/local/rvm/scripts/rvm
+#source /usr/local/rvm/scripts/rvm
 /etc/init.d/postgresql start
-/opt/msf/msfupdate --git-branch master
+#/opt/msf/msfupdate --git-branch master
 #/opt/msf/msfconsole
 #/bin/bash
 cd /opt/beef
+sed -i 's/\(\s\)passwd: "beef"/\1passwd: "docker_beef"/g' config.yaml
 ./beef
