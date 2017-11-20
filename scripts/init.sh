e322401d010d5c3ab4eb64e029631cbb165b8581
Matteo Rosi <mrosi@omniagroup.it>
2017-11-20 14:37:11 +0100

    change default credentials

--- scripts/init.sh
+++ scripts/init.sh
@@ -8,7 +8,7 @@
 cd /opt/beef
 sed -i 's/\(\s\)passwd: "beef"/\1passwd: "docker_beef"/g' config.yaml
 sed -i 's/user:   "beef"/user:   "admin"/g' config.yaml
-sed -i 's/#public: ""/public: "admin.beef"/g' config.yaml
-sed -i 's/#public_port: ""/public_port: "80"/g' config.yaml
-sed -i 's/debug: false/debug: true/g' config.yaml
+# sed -i 's/#public: ""/public: "admin.beef"/g' config.yaml
+# sed -i 's/#public_port: ""/public_port: "80"/g' config.yaml
+# sed -i 's/debug: false/debug: true/g' config.yaml
 bash -c "/opt/beef/beef -x -v -c /opt/beef/config.yaml"
